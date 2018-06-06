e238d973b923e7295dc03a949a1da4233f7aa829
zhaosong zhu <danielzhu1201@icloud.com>
2018-06-06 17:41:19 +0800

    can crawl info now

--- mobile/pipelines.py
+++ mobile/pipelines.py
@@ -5,9 +5,80 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from twisted.enterprise import adbapi
+import MySQLdb
+import MySQLdb.cursors
+from scrapy.crawler import Settings as settings
+import traceback
 
 class MobilePipeline(object):
-    def process_item(self, item, spider):
-	print "\n\n\n im in pipeline \n\n\n"
-	for it in item: print it
-    
+	def __init__(self):
+		conn = dict(
+			host = "localhost",
+			db = "zolmobile",
+			user = "root",
+			passwd = "12345678",
+			charset = "utf8",
+			cursorclass = MySQLdb.cursors.DictCursor,
+			use_unicode = True,
+		)
+		self.dbpool = adbapi.ConnectionPool("MySQLdb",**conn)
+
+	def process_item(self,item,spider):
+		if item['type'] == 'price':
+			res = self.dbpool.runInteraction(self.insert_into_table,item)
+		elif item['type'] == 'param':
+			res = self.dbpool.runInteraction(self.insert_into_videotable,item)
+		else:
+			pass
+
+	def insert_into_table(self,conn,item):
+	    try:
+		data = tuple([
+						item['type'],
+						item['source_url'],
+						item['proId'],
+						item['seriesId'],
+						item['subcateId'],
+						item['subcateName'],
+						item['manuId'],
+						item['manuName'],
+						item['breadcrumb'],
+						item['page_title'],
+						item['anothername'],
+						item['price'],
+						item['rate'],
+						item["commentCount"],
+						item['goodWords'],
+						item['badWords'],
+				])
+        	conn.execute(
+		    'insert into price(type,source_url,proId,seriesId,subcateId,subcateName,manuId,manuName,breadcrumb,page_title,anothername,price,rate,commentCount,goodWords,badWords) values("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s")' % data
+                    )
+					
+            except Exception as e:
+    		traceback.print_exc()
+
+	def insert_into_videotable(self,conn,item):
+		try:
+		
+			data = tuple([
+						item['_id'],
+						item['zolSubName'],
+						item['type'],
+						item['source_url'],
+						item['mobile_code'],
+						item['zolproductid'],
+						item['zolproduct'],
+						item['zolManuCnName'],
+						item['breadcrumb'],
+						item['page_title'],
+						item['spec'],
+			])
+
+			print len(data)
+        		conn.executemany(
+				'insert into info(_id,zolSubName,type,source_url,mobile_code,zolproductid,zolproduct,zolManuCnName,breadcrumb,page_title,spec) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)', [data, ]
+				)
+        	except Exception as e:
+        		traceback.print_exc()
--- mobile/spiders/zol.py
+++ mobile/spiders/zol.py
@@ -1,6 +1,7 @@
 #coding:utf-8
 import scrapy
 import re
+import json
 from scrapy import Spider
 from scrapy.selector import Selector
 from scrapy.spiders import CrawlSpider, Rule
@@ -61,13 +62,13 @@ class DatabloggerSpider(CrawlSpider):
         mobile_info_dict['proId']='|'.join(re.findall('proId\D*(\d*)',response.body)).replace("\n","")
         mobile_info_dict['seriesId']='|'.join(re.findall('seriesId\D*(\d*)',response.body)) #this one's right
         mobile_info_dict['subcateId']='|'.join(re.findall('subcateId\D*(\d*)',response.body)).replace("\n","")
-        mobile_info_dict['subcateName']='|'.join([line.strip() for line in re.findall('subcateName\D*(\s*)',response.body_as_unicode())]).replace("\n","")# dont work
+        mobile_info_dict['subcateName']='|'.join([line.strip() for line in re.findall(u'subcateName[\s，,：:\']*(\S*?)\'',response.body_as_unicode())]).replace("\n","")# dont work
         mobile_info_dict['manuId']='|'.join(re.findall('manuId\D*(\d*)',response.body)).replace("\n","")
-        mobile_info_dict['manuName']='|'.join([line.strip() for line in re.findall('manuName\s*= \'([^\']*)',response.body_as_unicode())]).replace("\n","")# dont work
+        mobile_info_dict['manuName']='|'.join([line.strip() for line in re.findall(u'manuName[\s，,：:\']*(\S*?)\'',response.body_as_unicode())]).replace("\n","")# dont work
         
         selector_res=Selector(response)
         mobile_info_dict['breadcrumb']='|'.join([line.strip() for line in selector_res.xpath('//div[@class="wrapper clearfix"]/div[@class="breadcrumb"]/a/text()').extract()]).replace("\n","")
-        mobile_info_dict['page-title']=''.join([line.strip() for line in selector_res.xpath('//div[contains(@class,"product-model page-title clearfix")]/h1/text()').extract()]).replace("\n","")
+        mobile_info_dict['page_title']=''.join([line.strip() for line in selector_res.xpath('//div[contains(@class,"product-model page-title clearfix")]/h1/text()').extract()]).replace("\n","")
         mobile_info_dict['anothername']=re.sub(u'\u522b\u540d\uff1a','',''.join([line.strip() for line in selector_res.xpath('//div[contains(@class,"page-title")]/h2/text()').extract()])).replace("\n","")
         mobile_info_dict['price']=''.join(selector_res.xpath('//div[@class="price price-normal"]/span/b[contains(@class,"price-type")]/text()').extract())
         mobile_info_dict['rate'] = "|".join([line.strip() for line in selector_res.xpath("//div[@class='review-comments-score clearfix']//div[@class='total-score']/strong/text()").extract()]).replace("\n","")
@@ -76,20 +77,17 @@ class DatabloggerSpider(CrawlSpider):
         mobile_info_dict['badWords'] = "|".join(selector_res.xpath("//div[@class='comments-words']//ul[@class='words-list clearfix']/li[@class='bad-words']/a/text()").extract()).replace("\n","")
 
         yield mobile_info_dict
-        #item2 = {}
-        #item2["hey"] = "halo"
-        #yield item2     # will be divided into two pipelines
-        # Only extract canonicalized and unique links of param (with respect to the current page)
+
+        #create new requests to search param
         links = LinkExtractor(allow = ('param.shtml',),canonicalize=True,unique=True).extract_links(response)
         for link in links:
             yield scrapy.Request(link.url, callback = self.parse2)
    
     def parse2(self, response):
-        print (response.url)
         print ("\n\n\n\n parse 2\n\n")
         # The list of items that are found on the particular page
         mobile_info_dict={}
-        mobile_info_dict["_id"] = response.url
+        mobile_info_dict['_id'] = response.url
         mobile_info_dict['zolSubName']='|'.join([line.strip().replace("\n","") for line in re.findall('var zolSubName\s*= \'([^\']*)',response.body_as_unicode())]) #can we do better than this?
         if mobile_info_dict['zolSubName']!=u'\u624b\u673a':yield {}  #"手机"的unicode
         mobile_info_dict['type']='param'
@@ -106,20 +104,27 @@ class DatabloggerSpider(CrawlSpider):
         selector_res=Selector(response)
 
         mobile_info_dict['breadcrumb']='|'.join([line.strip().replace("\n","") for line in selector_res.xpath('//div[@class="wrapper clearfix"]/div[@class="breadcrumb"]/a/text()').extract()])
-        mobile_info_dict['page-title']=''.join([line.strip().replace("\n","") for line in selector_res.xpath('//h1/text()').extract()])	
+        mobile_info_dict['page_title']=''.join([line.strip().replace("\n","") for line in selector_res.xpath('//h1/text()').extract()])	
 	#ok
+        #use another dictionary for spec
+        mobile_spec_dict = {}
         mobile_infos=selector_res.xpath('//div[@class="detailed-parameters"]/table/tr') #直接在tr下面找就行，要匹配newPmName 和 newPmVal
         for mobile_info in mobile_infos:
             mobile_info_tag=''.join([line.strip().replace("\n","") for line in mobile_info.xpath('th/span[contains(@id,"newPmName")]/text()').extract()])
 
-            mobile_info_value='|'.join([line.strip().replace("\n","") for line in mobile_info.xpath('td/span[contains(@id,"newPmVal")]/text()').extract()])
+            mobile_info_value_p1=[line.strip().replace("\n","") for line in mobile_info.xpath('td/span[contains(@id,"newPmVal")]/text()').extract()] #只要value有<a> 就不行
+            mobile_info_value_p2 = [line.strip().replace("\n","") for line in mobile_info.xpath('td/span[contains(@id,"newPmVal")]/a/text()').extract()]
+            mobile_info_value = [re.sub(u'^，', '', re.sub(u"，$", '', keyword)) for keyword in mobile_info_value_p1] + mobile_info_value_p2
+            mobile_info_value='|'.join([_item for _item in mobile_info_value if _item])
 
             if mobile_info_tag!=u'\u8be6\u7ec6\u5185\u5bb9' and mobile_info_value: pass	
 	        #else:
             #    print 666
                 #mobile_info_value='|'.join([line.strip().replace("\n","") for line in mobile_info.xpath('td/span[contains(@id,"newPmVal")]/text()').extract()])
             
-            mobile_info_dict[mobile_info_tag]=mobile_info_value
+            mobile_spec_dict[mobile_info_tag]=mobile_info_value
+
+        mobile_info_dict['spec'] = json.dumps(mobile_spec_dict) #dump all spec out with json.dumps
 
         yield mobile_info_dict
         # Only extract canonicalized and unique links (with respect to the current page)
